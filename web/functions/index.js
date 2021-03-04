6b77e95fa765e6172386fa4fb823f34f48752a23
Edward Alexis <edalvb@gmail.com>
2021-03-04 11:45:55 -0500

    Creación de un usuario con Cloud Functions

--- web/functions/index.js
+++ web/functions/index.js
@@ -1,9 +1,31 @@
 const functions = require("firebase-functions");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://gca-eventos-default-rtdb.firebaseio.com"
+});
+
+
+exports.onCreateEvent = functions.firestore
+    .document('eventos/{eventoId')
+    .onCreate(async (snap, context) => {
+        const evento = snap.data;
+
+        try {
+            var user = await admin.auth()
+                .createUser({
+                    email: evento.organizador,
+                    password: 'secret' + Math.floor(Math.random() * 999)
+                });
+            console.log(user);
+        } catch (err) {
+            console.log("Error", err.message);
+        }
+
+        return 0;
+
+    });
