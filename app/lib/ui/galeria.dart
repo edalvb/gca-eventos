2be80b93715cd7dabf8ef7d70949ba987a2f0ec6
Edward Alexis <edalvb@gmail.com>
2021-04-26 17:51:59 -0500

    Pantalla galería

--- app/lib/ui/galeria.dart
+++ app/lib/ui/galeria.dart
@@ -1,15 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:app/models/evento.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-class GaleriaPage extends StatelessWidget {
+class GaleriaPage extends StatefulWidget {
   final Evento evento;
 
   GaleriaPage({this.evento});
 
   @override
+  _GaleriaPageState createState() => _GaleriaPageState();
+}
+
+class _GaleriaPageState extends State<GaleriaPage> {
+  final firestore = FirebaseFirestore.instance;
+
+  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Galeria'),
+    return StreamBuilder(
+      stream: firestore
+          .collection("eventos/${widget.evento.id}/gallery")
+          .snapshots(),
+      builder: (_, AsyncSnapshot<QuerySnapshot> snapshot) {
+        var imageDocs = snapshot.data?.docs ?? [];
+        var imageList = imageDocs
+            .map((doc) => FadeInImage.assetNetwork(
+                  image: doc.get("url"),
+                  placeholder: 'assets/cargando.gif',
+                  fit: BoxFit.cover,
+                ))
+            .toList();
+        return GridView.count(
+          crossAxisCount: 2,
+          crossAxisSpacing: 8,
+          mainAxisSpacing: 8,
+          padding: EdgeInsets.all(8),
+          children: imageList,
+        );
+      },
     );
   }
 }
